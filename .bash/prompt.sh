e25385b65bbbf0aa8ff46756a446314b7a93ce48
Remco Wendt <remco@maykinmedia.nl>
2011-10-14 11:04:46 +0200

    Adapted prompt to show hostname in different color when on a remote host

--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -1,21 +1,31 @@
-MITSUHIKOS_DEFAULT_COLOR="[00m"
-MITSUHIKOS_GRAY_COLOR="[37m"
-MITSUHIKOS_PINK_COLOR="[35m"
-MITSUHIKOS_GREEN_COLOR="[32m"
-MITSUHIKOS_ORANGE_COLOR="[33m"
-MITSUHIKOS_RED_COLOR="[31m"
-if [ `id -u` == '0' ]; then
-  MITSUHIKOS_USER_COLOR=$MITSUHIKOS_RED_COLOR
-else
-  MITSUHIKOS_USER_COLOR=$MITSUHIKOS_PINK_COLOR
-fi
-MITSUHIKOS_VCPROMPT_EXECUTABLE=~/.dotfiles/bin/vcprompt
+# Based on Mitshuhiko's dotfiles: https://github.com/mitsuhiko/dotfiles
 
-mitsuhikos_vcprompt() {
-  $MITSUHIKOS_VCPROMPT_EXECUTABLE -f $' on \033[34m%n\033[00m:\033[00m%b\033[32m%m%u'
+PROMPT_DEFAULT_COLOR="[00m"
+PROMPT_GRAY_COLOR="[37m"
+PROMPT_PINK_COLOR="[35m"
+PROMPT_GREEN_COLOR="[32m"
+PROMPT_ORANGE_COLOR="[33m"
+PROMPT_RED_COLOR="[31m"
+if [ `id -u` == '0' ]; then
+    PROMPT_USER_COLOR=$PROMPT_RED_COLOR
+else
+    PROMPT_USER_COLOR=$PROMPT_PINK_COLOR
+fi
+# Check if we are connect to a remote server and change color of 
+# machine name
+if [ -n "${SSH_CLIENT}" ]; then
+    PROMPT_HOST_COLOR=$PROMPT_RED_COLOR
+else
+    PROMPT_HOST_COLOR=$PROMPT_ORANGE_COLOR
+fi
+
+PROMPT_VCPROMPT_EXECUTABLE=~/.dotfiles/bin/vcprompt
+
+prompt_vcprompt() {
+  $PROMPT_VCPROMPT_EXECUTABLE -f $' on \033[34m%n\033[00m:\033[00m%b\033[32m'
 }
 
-mitsuhikos_lastcommandfailed() {
+prompt_lastcommandfailed() {
   code=$?
   if [ $code != 0 ]; then
     echo -n $'\033[37m exited \033[31m'
@@ -24,21 +34,21 @@ mitsuhikos_lastcommandfailed() {
   fi
 }
 
-mitsuhikos_virtualenv() {
+prompt_virtualenv() {
   if [ x$VIRTUAL_ENV != x ]; then
-    echo -n $' \033[37menv \033[31m'
+    echo -n $' \033[37menv \033[36m'
     basename "${VIRTUAL_ENV}"
     echo -n $'\033[00m'
   fi
 }
 
-export MITSUHIKOS_BASEPROMPT='\n\e${MITSUHIKOS_USER_COLOR}\u\
-\e${MITSUHIKOS_GRAY_COLOR} at \e${MITSUHIKOS_ORANGE_COLOR}\h \
-\e${MITSUHIKOS_GRAY_COLOR}in \e${MITSUHIKOS_GREEN_COLOR}\w\
-`mitsuhikos_lastcommandfailed`\
-\e${MITSUHIKOS_GRAY_COLOR}`mitsuhikos_vcprompt`\
-`mitsuhikos_virtualenv`\
-\e${MITSUHIKOS_DEFAULT_COLOR}'
-export PS1="${MITSUHIKOS_BASEPROMPT}
+export PROMPT_BASEPROMPT='\n\e${PROMPT_USER_COLOR}\u\
+\e${PROMPT_GRAY_COLOR} at \e${PROMPT_HOST_COLOR}\h \
+\e${PROMPT_GRAY_COLOR}in \e${PROMPT_GREEN_COLOR}\w\
+`prompt_lastcommandfailed`\
+\e${PROMPT_GRAY_COLOR}`prompt_vcprompt`\
+`prompt_virtualenv`\
+\e${PROMPT_DEFAULT_COLOR}'
+export PS1="${PROMPT_BASEPROMPT}
 $ "
 
