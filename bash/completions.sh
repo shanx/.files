6454b7ff1a34e67949a9c26a4f12fbf65386802e
Remco Wendt <remco@maykinmedia.nl>
2011-10-20 02:30:31 +0200

    Upated local bash completions

--- bash/completions.sh
+++ bash/completions.sh
@@ -1,6 +1,11 @@
 source ~/.dotfiles/bash/completions/virtualenv.sh
 source ~/.dotfiles/bash/completions/django.sh
 
+# Different local bash completions
 if [ -f /opt/local/etc/bash_completion ]; then
   . /opt/local/etc/bash_completion
+fi
+
+if [ -f /etc/bash_completion ] && ! shopt -oq posix; then
+    . /etc/bash_completion
 fi
