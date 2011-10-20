68b81258425ab7a1d5ff927685042e29f20ec002
Remco Wendt <remco@maykinmedia.nl>
2011-10-20 02:24:55 +0200

    Of course at first install the bin path is not available, so use absolute ref

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ virtualenv ~/.dotfiles/.venv --no-site-packages
 ~/.dotfiles/.venv/bin/pip install dotfiles
 #ln -s ~/.dotfiles/.venv/bin/dotfiles ~/.dotfiles/bin/dotfiles
 ln -s ~/.dotfiles/dotfilesrc ~/.dotfilesrc
-dotfiles --sync
+~/.dotfiles/bin/dotfiles --sync
 
 # Create last_updated file
 touch ~/.dotfiles/.last_updated
