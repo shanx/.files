d376e488ea2f0e43dca373a3bee3c86f872d4f71
Remco Wendt <remco@maykinmedia.nl>
2011-11-01 14:59:05 +0100

    Better path handling, replaced pathmunge with code from superuser.com

--- bash/paths.sh
+++ bash/paths.sh
@@ -1,20 +1,6 @@
-# From: http://www.moosechips.com/2011/01/bash-pathmunge/
-# Changed path of egrep from /bin/egrep to /usr/bin/egrep
-pathmunge () {
-        if ! echo $PATH | `type egrep` -q "(^|:)$1($|:)" ; then
-           if [ "$2" = "after" ] ; then
-              PATH=$PATH:$1
-           else
-              PATH=$1:$PATH
-           fi
-        fi
-}
-
-pathmunge ~/.dotfiles/bin/
+add_to_path ~/.dotfiles/bin/
 
 # Include local bin directory in path if it exists
 if [ -d ~/bin ] ; then
-    pathmunge ~/bin
+    add_to_path ~/bin
 fi
-
-export PATH
