b6e4f5c2938acc29f67a4571e03b411cc7728818
Remco Wendt <remco@maykinmedia.nl>
2012-08-29 19:29:59 +0200

    Added easier function for cding up

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -16,3 +16,26 @@ alias la='ls -als'
 
 # Some env's seem to serve a different vim when starting with vi. So just alias to be sure
 alias vi='vim'
+
+# Go up directory tree X number of directories
+function up() {
+    COUNTER="$1";
+    # default $COUNTER to 1 if it isn't already set
+    if [[ -z $COUNTER ]]; then
+        COUNTER=1
+    fi
+    # make sure $COUNTER is a number
+    if [ $COUNTER -eq $COUNTER 2> /dev/null ]; then
+        nwd=`pwd` # Set new working directory (nwd) to current directory
+        # Loop $nwd up directory tree one at a time
+        until [[ $COUNTER -lt 1 ]]; do
+            nwd=`dirname $nwd`
+            let COUNTER-=1
+        done
+            cd $nwd # change directories to the new working directory
+    else
+        # print usage and return error
+        echo "usage: up [NUMBER]"
+        return 1
+    fi
+}
