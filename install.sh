5c9edb8291be57fbd2edcc00e392612316b900c1
Remco Wendt <remco.wendt@gmail.com>
2014-05-25 14:34:46 +0200

    Remove dotfiles python auto link thingy, replaced with manual symlinks

--- install.sh
+++ install.sh
@@ -7,14 +7,24 @@ pushd external/hg-prompt/
 git submodule update --init
 popd
 
-# Install .files
-virtualenv ~/.files/.venv --no-site-packages
-~/.files/.venv/bin/pip install dotfiles
-ln -sf ~/.files/dotfilesrc ~/.dotfilesrc
-~/.files/bin/dotfiles --sync
+# Install .files by manually symlinking files 
+pushd ~
+ln -si .files/buildout .buildout
+ln -si .files/config .config
+ln -si .files/dir_colors .dir_colors
+ln -si .files/gitconfig .gitconfig
+ln -si .files/gitignore_global .gitignore_global
+ln -si .files/hgignore_global .hgignore_global
+ln -si .files/hgrc .hgrc
+ln -si .files/hushlogin .hushlogin
+ln -si .files/inputrc .inputrc
+ln -si .files/slate .slate
+ln -si .files/tmux.conf .tmux.conf
+ln -si .files/vim .vim
+ln -si .files/vimrc .vimrc
+ln -si .files/zshrc .zshrc
+popd
 
 # Install vundle bundles
 vim -c "BundleInstall"
 
-# Create last_updated file
-touch ~/.files/.last_updated
